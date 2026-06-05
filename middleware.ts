f3b05caa0b387e6db642ed1ac3ad7bef9c8612a6
elifnalan <emutlu1@cougars.ccis.edu>
2026-06-04 23:53:15 -0500

    add clerk auth, sign-in and sign-up pages

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+/* Checking the routes and displays them according to the public/private pages  */
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
